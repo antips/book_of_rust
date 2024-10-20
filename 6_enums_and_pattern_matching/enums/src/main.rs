bdb2cb5b1698c53695f1aec169dc757faa5bbdb6
DEWERDT Antoine <antoinecodew@gmail.com>
2024-10-20 19:33:23 +0200

    Chapter 6: Enum declaration

--- 6_enums_and_pattern_matching/enums/src/main.rs
+++ 6_enums_and_pattern_matching/enums/src/main.rs
@@ -1,3 +1,13 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
 fn main() {
-    println!("Hello, world!");
+    basic_enum_usage();
+}
+
+fn basic_enum_usage() {
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
 }

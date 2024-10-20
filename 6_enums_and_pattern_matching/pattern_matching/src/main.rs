31e4ba5032be7159724d789ee86bcbf5f1272059
DEWERDT Antoine <antoinecodew@gmail.com>
2024-10-20 23:30:58 +0200

    Chapter 6: pattern matching with enum value

--- 6_enums_and_pattern_matching/pattern_matching/src/main.rs
+++ 6_enums_and_pattern_matching/pattern_matching/src/main.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    _Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     _Nickel,
     _Dime,
-    _Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -13,16 +20,26 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::_Nickel => 5,
         Coin::_Dime => 10,
-        Coin::_Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
     }
 }
 
 fn main() {
     basic_pattern_matching();
+    pattern_matching_with_enum_value();
 }
 
 fn basic_pattern_matching() {
     let penny = Coin::Penny;
     let result = value_in_cents(penny);
-    println!("The result value is {result}");
+    println!("The result value of Penny is {result}");
+}
+
+fn pattern_matching_with_enum_value() {
+    let quarter = Coin::Quarter(UsState::Alabama);
+    let result = value_in_cents(quarter);
+    println!("The result value of Quarter is {result}");
 }

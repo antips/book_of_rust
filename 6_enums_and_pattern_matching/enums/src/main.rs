8cdde9c5bee5e3f7f2d592bd28f1ad2aeeacf411
DEWERDT Antoine <antoinecodew@gmail.com>
2024-10-20 20:13:05 +0200

    Chapter 6: the Option type

--- 6_enums_and_pattern_matching/enums/src/main.rs
+++ 6_enums_and_pattern_matching/enums/src/main.rs
@@ -25,6 +25,8 @@ fn main() {
     basic_enum_usage();
     store_value_in_enum();
     enum_can_implement_methods();
+    the_builtin_option_enum();
+    option_type_cannot_be_used_as_valid_value();
 }
 
 fn basic_enum_usage() {
@@ -40,4 +42,18 @@ fn store_value_in_enum() {
 fn enum_can_implement_methods() {
     let m = Message::Write(String::from("hello"));
     m.call();
+}
+
+fn the_builtin_option_enum() {
+    let _some_number = Some(5); // Implicit type: Option<i32>
+    let _some_char = Some('e'); // Implicit type: Option<char>
+
+    let _absent_number: Option<i32> = None;
+}
+
+fn option_type_cannot_be_used_as_valid_value() {
+    let _x: i8 = 5;
+    let _y: Option<i8> = Some(5);
+
+    // let sum = x + y; -> would not compile because y could be `None`
 }

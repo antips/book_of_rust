a3de1597f2b80e3408553c23c59ea9f6d7ce38d2
DEWERDT Antoine <antoinecodew@gmail.com>
2024-10-20 18:03:06 +0200

    Chapter 5: structs methods

--- 5_structs/rectangles/src/main.rs
+++ 5_structs/rectangles/src/main.rs
@@ -4,29 +4,54 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        // Associated function because no '&self' argument. Usage syntax: 'Rectagle::square(size)'
+        // Can be used as constructors, like here:
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let square = Rectangle::square(60);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
 
-    println!("rect1 is {rect1:?}"); // The {:?} format enables using the Debug trait to DEBUG the struct properties.
-    println!("rect1 is {rect1:#?}"); // The {:#?} format enables using the Debug trait to PRETTY PRINT the struct properties.
+    debug_usage();
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold square? {}", rect1.can_hold(&square));
+}
 
+fn debug_usage() {
     let scale = 2;
-    let rect2 = Rectangle {
+    let rect_debug = Rectangle {
         width: dbg!(30 * scale),
         height: 50,
     };
+    dbg!(&rect_debug); // Pretty prints, but prints in STDERR instead and TAKES (and then RETURNS) the value OWNERSHIP
 
-    dbg!(&rect2); // Pretty prints, but prints in STDERR instead and TAKES (and then RETURNS) the value OWNERSHIP
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("rect_debug is {rect_debug:?}"); // The {:?} format enables using the Debug trait to DEBUG the struct properties.
+    println!("rect_debug is {rect_debug:#?}"); // The {:#?} format enables using the Debug trait to PRETTY PRINT the struct properties.
 }

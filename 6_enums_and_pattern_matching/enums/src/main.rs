c7502ebf7792f38cf1660e5f0e577ba1e5c48171
DEWERDT Antoine <antoinecodew@gmail.com>
2024-10-20 19:51:13 +0200

    Chapter 6: enum objects can store values as well

--- 6_enums_and_pattern_matching/enums/src/main.rs
+++ 6_enums_and_pattern_matching/enums/src/main.rs
@@ -3,11 +3,22 @@ enum IpAddrKind {
     V6,
 }
 
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
 fn main() {
     basic_enum_usage();
+    store_value_in_enum();
 }
 
 fn basic_enum_usage() {
     let _four = IpAddrKind::V4;
     let _six = IpAddrKind::V6;
+}
+
+fn store_value_in_enum() {
+    let _home = IpAddr::V4(127, 0, 0, 1);
+    let _loopback = IpAddr::V6(String::from("::1"));
 }
